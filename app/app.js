92b1c1178ac8c243c48d79817c55aedf2884f7ab
favour <aransiolafavour@yahoo.com>
2022-10-10 09:58:54 +0100

    changes

--- app/app.js
+++ app/app.js
@@ -1,4 +1,27 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -16,12 +39,15 @@ const express_1 = __importDefault(require("express"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const todos_route_1 = __importDefault(require("./routes/todos_route"));
+const dotenv = __importStar(require("dotenv"));
 const app = (0, express_1.default)();
 app.use(body_parser_1.default.json());
 app.use(todos_route_1.default);
+dotenv.config();
 const port = 3000;
 const main = () => __awaiter(void 0, void 0, void 0, function* () {
-    yield mongoose_1.default.connect('mongodb+srv://taiye:taiye2000@serverlessinstance0.4jcsv.mongodb.net/practice?retryWrites=true&w=majority');
+    const DATABASE_URI = process.env.DATABASE_URI;
+    yield mongoose_1.default.connect(DATABASE_URI);
     console.log('Server Started');
     app.listen(port);
 });
--- src/app.ts
+++ src/app.ts
@@ -2,16 +2,19 @@ import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import todoRoute from './routes/todos_route';
+import * as dotenv from 'dotenv';
 
 const app = express();
 app.use(bodyParser.json());
 app.use(todoRoute);
+dotenv.config();
 
 const port = 3000;
 
 
 const main = async () => {
-    await mongoose.connect('mongodb+srv://taiye:taiye2000@serverlessinstance0.4jcsv.mongodb.net/practice?retryWrites=true&w=majority');
+    const DATABASE_URI = process.env.DATABASE_URI as string;
+    await mongoose.connect(DATABASE_URI);
     console.log('Server Started')
     app.listen(port)
 }
